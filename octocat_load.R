89a07e3d9b81bd479f207c7e864dd246b78bfa6a
Tulip Daaboul <tdaaboul@smith.edu>
2023-01-30 13:09:25 -0500

    fixed reading in octocat again

--- octocat_load.R
+++ octocat_load.R
@@ -1,7 +1,10 @@
 # Code to load in Octocat (github mascot) art
 
 # replaced old readLines to load from local version and not internet
-octocat = readLines("./octocat.txt")
+
+#fixed again 
+octocat = readLines("https://raw.githubusercontent.com/Adv-R-Programming/Adv-R-Reader/main/content/class_worksheets/02_git/octocat.txt")
+
 
 
 writeLines(octocat, "./octocat.txt")
